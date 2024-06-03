e686f45a4de6b8c4816903ed779bc9dc79e41091
DESKTOP-DIHB611\ADMIN <phuongthaoybyb@gmail.com>
2024-06-03 22:04:07 +0700

    add request

--- src/main/java/vn/fpt/diamond_shop/request/AddCartRequest.java
+++ src/main/java/vn/fpt/diamond_shop/request/AddCartRequest.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddCartRequest extends BaseRequest{
+    private Long customerId;
     private Long jewelryId;
     private Integer quantity;
     private String status;
